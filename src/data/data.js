525b9127f1f8430e3ee6df597e5042a816dfc31a
roro <robin.roeoesli@bluewin.ch>
2023-08-15 17:45:18 +0200

    updated tasks

--- src/data/data.js
+++ src/data/data.js
@@ -1,51 +1,77 @@
-export const data = [{
-  "workingStream": "Admin",
-  "tasks": [
-    "SPARC-2681 - Weekly TIE International", 
-    "SPARC-2429 - A.2.1.4 Synchronisation mit Auftraggeberschaft (bi-weekly) PI 4",
-    "SPARC-2670 - Pre-Planning Preperation Tie International",
-    "SPARC-2671 - Roadmap Task Creation & Analysis"
-  ]
-},
-{
-  "workingStream": "Content EDU",
-  "tasks": [
-    "SPARC-2366 - C.3.7 - Windows Engineering Basics (16 h)",
-    "SPARC-2663 - Weiterentwicklung Network Engineering Basics",
-    "SPARC-2706 - Linux Engineering Basics - Labs",
-    "SPARC-2733 - Programming Basics in Python - Labs Cleanup",
-  ]
-},
-{
-  "workingStream": "Event Organisation",
-  "tasks": [
-    "SPARC-2587 - EO.4.3 - Allgemeine Planung Erlebnistage",
-    "SPARC-2657 - Erlebnistage Module x1 Collaborate \"Build a Firewall and Routing\"",
-    "SPARC-2665 - T-Shirts & Hoodies",
-    "SPARC-2707 - Vorbereitung CONNECTED",
-    "SPARC-2712 - Auf- und Abbau CONNECTED - TIE",
-    "SPARC-2708 - Durchführung CONNECTED - TIE",
-    "SPARC-2710 - Nachbearbeitung CONNECTED - TIE"
-  ]
-},
-{
-  "workingStream": "Hosted Infrastructure",
-  "tasks": ["SPARC-2701 - Website"]
-},
-{
-  "workingStream": "Membership Management",
-  "tasks": [
-    "SPARC-2590 - MM.6.2 - Betreuungs- und Supportkonzept erstellen",
-    "SPARC-2662 - Newsletter Tool",
-    "SPARC-2734 - Support (email und efront)",
-    "SPARC-2740 - Communication VACy Members"
-  ]
-},
-{
-  "workingStream": "Public Relations",
-  "tasks": [
-    "SPARC-2700 - Kommunikative Grundlagen",
-    "SPARC-2703 - Paid Ads",
-    "SPARC-2705 - Social Media Set-Up"
-  ]
-}];
+[
+  {
+    "workingStream": "Admin",
+    "tasks": [
+      "SPARC-2670 - Pre-Planning Preperation Tie International",
+      "SPARC-2681 - Weekly TIE International",
+      "SPARC-2753 - August Weekly TIE",
+      "SPARC-2754 - September Weekly TIE",
+      "SPARC-2755 - Oktober Weekly TIE",
+      "SPARC-2791 - Reisezeit PI 4 TIE International"
+    ]
+  },
+  {
+    "workingStream": "Content EDU",
+    "tasks": [
+      "SPARC-2366 - C.3.7 - Windows Engineering Basics (16 h)",
+      "SPARC-2783 - WinENGBasic - Complete Course Outlines",
+      "SPARC-2663 - Weiterentwicklung Network Engineering Basics",
+      "SPARC-2706 - Linux Engineering Basics - Labs",
+      "SPARC-2726 - Create Overview for Lab Units",
+      "SPARC-2727 - Create Skeleton for the Linux Hands-On Lab(s)",
+      "SPARC-2733 - Programming Basics in Python - Labs Cleanup",
+      "SPARC-2726 - Create Overview for Lab Units",
+      "SPARC-2727 - Create Skeleton for the Linux Hands-On Lab(s)",
+      "SPARC-2783 - WinENGBasic - Complete Course Outlines"
+    ]
+  },
+  {
+    "workingStream": "Event Organisation",
+    "tasks": [
+      "SPARC-2587 - EO.4.3 - Allgemeine Planung Erlebnistage",
+      "SPARC-2604 - EO.4.3.a - Checklist für Organisation erstellen",
+      "SPARC-2613 - EO.4.3.b - Aufsetzen der Qualitätssicherungsinstrumente Erlebnistage",
+      "SPARC-2622 - EO.4.3.c - Ausbildung Personal für Erlebnistage",
+      "SPARC-2623 - EO.4.3.d - Material für Erlebnistage definieren und organisieren",
+      "SPARC-2608 - EO.4.3.e - Ergänzung Notfallkonzept um für Erlebnistage relevante Punkte",
+      "SPARC-2614 - EO.4.3.x - Planung der Erlebnistage PI4 & PI5",
+      "SPARC-2657 - Erlebnistage Module x1 Collaborate \"Build a Firewall and Routing\"",
+      "SPARC-2707 - Vorbereitung CONNECTED",
+      "SPARC-2712 - Auf- und Abbau CONNECTED - TIE",
+      "SPARC-2710 - Nachbearbeitung CONNECTED - TIE",
+      "SPARC-2712 - Auf- und Abbau CONNECTED - TIE",
+      "SPARC-2710 - Nachbearbeitung CONNECTED - TIE",
+      "SPARC-2795 - Organisation erster Erlebnistag 2023"
+    ]
+  },
+  {
+    "workingStream": "Membership Management",
+    "tasks": [
+      "SPARC-2590 - MM.6.2 - Betreuungs- und Supportkonzept erstellen",
+      "SPARC-2662 - Newsletter Tool",
+      "SPARC-2736 - August 1st Level TIE",
+      "SPARC-2737 - August 2nd Level TIE",
+      "SPARC-861n6dv6c - September 1st Level Support",
+      "SPARC-861n6dvr2 - September 2nd Level TIE",
+      "SPARC-2757 - Oktober 1st Level TIE",
+      "SPARC-2759 - Oktober 2nd Level TIE",
+      "SPARC-2762 - Aug - Vacy Comm",
+      "SPARC-2763 - Sep - Vacy Comm",
+      "SPARC-2764 - Okt - Vacy Comm"
+    ]
+  },
+  {
+    "workingStream": "Public Relations",
+    "tasks": [
+      "SPARC-2700 - Kommunikative Grundlagen",
+      "SPARC-2703 - Paid Ads",
+      "SPARC-2705 - Social Media Set-Up",
+      "SPARC-2789 - September Website überarbeitung",
+      "SPARC-2701 - Website",
+      "SPARC-2788 - August Website überarbeitung",
+      "SPARC-2790 - Oktober Website überarbeitung",
+      "SPARC-2788 - August Website überarbeitung",
+      "SPARC-2790 - Oktober Website überarbeitung"
+    ]
+  }
+]
