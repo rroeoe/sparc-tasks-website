abdb72d6183257cc8030d3ee130ded384611e477
roro <robin.roeoesli@bluewin.ch>
2024-01-18 10:27:59 +0100

    new tasks

--- src/data/data.js
+++ src/data/data.js
@@ -2,55 +2,66 @@ export const data = [
   {
     "workingStream": "Admin (SPARC)",
     "tasks": [
-      "SPARC-2893 - TIE Reisezeit PI5",
-      "SPARC-2902 - Weekly TIE International",
-      "SPARC-2905 - November Weekly TIE",
-      "SPARC-2904 - December Weekly TIE",
-      "SPARC-2928 - TIE Steering Meeting PI 5"
+      "SPARC-3007 - TIE PI Planning",
+      "SPARC-3008 - TIE PI Planning Preparation",
+      "SPARC-3009 - TIE PI Planning Meetings",
+      "SPARC-3010 - TIE Steering Meeting",
+      "SPARC-3011 - Weekly TIE International",
+      "SPARC-3013 - Weekly TIE Jan 2024",
+      "SPARC-3014 - Weekly TIE Feb 2024",
+      "SPARC-3016 - Weekly TIE Mar 2024",
+      "SPARC-3022 - TIE Projektmeeting Reisezeit",
+      "SPARC-3031 - Bi-Weekly Team Meeting TIE",
+      "SPARC-3032 - Bi-Weekly TIE Jan 2024",
     ]
   },
   {
     "workingStream": "Content EDU (SPARC)",
     "tasks": [
-      "SPARC-2889 - Content Module 2",
-      "SPARC-2890 - Content Module 3",
-      "SPARC-2891 - Content Module 4",
-      "SPARC-2892 - Content Module 5",
-      "SPARC-2926 - Aufsetzen Sandbox"
+      "SPARC-2976 - Finalisierung Windows Engineering Basics",
+      "SPARC-2926 - Aufsetzen Sandbox",
+      "SPARC-2989 - Content Überarbeitung",
+      "SPARC-3084 - Rework Lab Description eFront",
+      "SPARC-3015 - Update Python Labs",
+      "SPARC-3024 - Provide Hints for Python Challenges",
+      "SPARC-3023 - Python Challenges Server Prompt is incorrect"
     ]
   },
   {
     "workingStream": "Event Organisation (SPARC)",
     "tasks": [
-      "SPARC-2860 - Grobplanung Erlebnistage 2024",
-      "SPARC-2923 - Org. Erlebnistage Q1 2024 November",
-      "SPARC-2924 - Org. Erlebnistage Q1 2024 December",
-      "SPARC-2862 - Durchführung Erlebnistag 02.12.2023 in Zürich - TIE",
-      "SPARC-2863 - Vorbereitungen Erlebnistag 02.12.2023 in Zürich - TIE",
-      "SPARC-2864 - Briefing Erlebnistag 02.12.2023 in Zürich - TIE",
-      "SPARC-2871 - Nachbearbeitung Erlebnistag 02.12.2023 in Zürich - TIE",
-      "SPARC-2879 - Modul Build a Private Network testen",
-      "SPARC-2897 - Grobplanung Onboarding Session 2024"
+      "SPARC-2973 - Vorbereitung Erlebnistag Bern - TIE",
+      "SPARC-2975 - Überarbeitung BPAN Modul",
+      "SPARC-2981 - Durchführung Onboarding-Events - TIE",
+      "SPARC-2984 - Durchführung Erlebnistag Bern - TIE",
+      "SPARC-2985 - Nachbearbeitung Erlebnistag Bern - TIE",
+      "SPARC-2986 - Vorbereitung Erlebnistag Lausanne - TIE",
+      "SPARC-2987 - Durchführung Erlebnistag Lausanne - TIE",
+      "SPARC-2988 - Nachbearbeitung Erlebnistag Lausanne - TIE",
+      "SPARC-2993 - Industrialisierung Erlebnistage",
+      "SPARC-2994 - Planung Erlebnistage PI7"
     ]
   },
   {
     "workingStream": "Membership Management (SPARC)",
     "tasks": [
-      "SPARC-2880 - Evaluation und Testversuche Plattform Erklärvideos",
-      "SPARC-2884 - November VACy Comm",
-      "SPARC-2885 - December VACy Comm",
-      "SPARC-2887 - Support November",
-      "SPARC-2888 - Support December",
-      "SPARC-2872 - Weiterentwicklung Website"
+      "SPARC-2978 - Support für TIE Kurse",
+      "SPARC-3038 - Support TIE Kurse - Januar",
+      "SPARC-3039 - Support TIE Kurse - Februar",
+      "SPARC-3040 - Support TIE Kurse - März",
+      "SPARC-3041 - VACy Comm"
     ]
   },
   {
     "workingStream": "Public Relations (SPARC)",
     "tasks": [
-      "SPARC-2713 - PR.3.2 - Zusammenarbeit ChanceArmee",
-      "SPARC-2894 - Newsletter Tool Rampup",
       "SPARC-2895 - Paid Ads",
-      "SPARC-2896 - Social Media Setup"
+      "SPARC-2974 - Mediale Begleitung Erlebnistag",
+      "SPARC-2977 - Erklärvideos",
+      "SPARC-2980 - Aufnahme Info-Session",
+      "SPARC-2992 - Prep Onboarding Events",
+      "SPARC-3036 - Portrait Teilnehmende",
+      "SPARC-3037 - Anpassungen Website"
     ]
   }
 ]
